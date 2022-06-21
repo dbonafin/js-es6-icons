421edbde6d2527562ef26cc528a776d85a322b13
dbonafin <demianbonafina@gmail.com>
2022-06-21 22:15:28 +0200

    final layout done

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const icons = [
+const allIcons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -114,24 +114,88 @@ const icons = [
 ];
 
 
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona
-// In cui è presente il nome dell'icona e l'icona stessa.
+// GENERATORE COLORE RANDOM PER OGNI ELEMENTO DELLE CARDS 
+allIcons.forEach((element) => {
+	element.color = generateColor();
+});
 
-// Utilizzare "forEach" per l'array "icons", dove ogni singolo elemento sta per ogni singola icona
-// Il nome dell'icona è già presente nella key "name"
-// L'icona va assemblata, facendo la somma di "family" + spazio + "prefix" + "name" (ad es. fas fa-dog)
+// Richiamo il mio elemento HTML a cui voglio inserire tutte le mie cards
+const iconContainer = document.querySelector('.js-all-card'); 
 
-// Milestone 2
-// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-// Il colore blue sta alla categoria user
-// Il colore orange sta alla categoria animal
-// Il colore green sta alla categoria vegetable
+// FUNCTION PER POPOLARE IL DOM
+printCards(allIcons, iconContainer);
 
 
-// Milestone 3
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). 
-// Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-// Al click sullo specifico value della lista di opzioni
-// visualizzare solo gli elementi che corrispondono al colore di quella categoria (vedi milestone 2)
+// ----------------------
+//     EVENT CHANGE
+// ----------------------
+// CATEGORIZZO LE CARDS DA STAMPARE IN BASE ALLA SCELTA DELL'UTENTE 
+const selectCategory = document.querySelector('#category'); 
+// Reset dei valori a All per ogni volta che l'utente fa refresh della pagina
+selectCategory.value = 'all';
 
+selectCategory.addEventListener('change',
+	function() {
+
+		// Rendo leggibile la scelta dell'utente
+		const userValue = this.value;
+		// Svuoto il popolamento delle pagine ogni volta che l'utente cambia la sua categoria
+		iconContainer.innerHTML = '';
+
+		// Se nei dati dell'oggetto non e' compreso il type ALL allora 
+		if(userValue !== 'all') {
+			const filteredIcons = allIcons.filter((element) => {
+				return element.type === userValue;
+			});
+
+			// Popolazione DOM
+			printCards(filteredIcons, iconContainer);
+		} else {
+			// Popolazione DOM
+			printCards(allIcons, iconContainer); 
+		}
+	}
+);
+
+
+// --------------------
+// 		FUNCTION
+// --------------------
+// Popolazione DOM
+function printCards(array, container) {
+	array.forEach((element) => { 
+		const {name, prefix, type, family, color} = element; 
+	
+		const newCard = `
+		<div class="card flex" style="color: ${color}">
+			<i class="${family} ${prefix}${name}"></i>
+			<span>${name}</span>
+		</div>
+		`
+		container.innerHTML += newCard; 
+	}); 
+};
+
+// Generatore colore casuale
+function generateColor() {
+	let color = '#';
+	
+	// Generiamo una stringa che sia della stessa lunghezza di: #1b2b3a (esadecimale) 
+	// Variante a stringa con tutti i numeri a nostra disposizione
+	const symbols = '0123456789abcdef';
+	
+	for(let i = 0; i < 6; i++) {
+		const randomIndex = getRndInteger(0, symbols.length - 1);
+
+		const randomSymbol = symbols[randomIndex];
+
+		color += randomSymbol;
+	}
+	return color;
+};
+
+
+// Generatore numero random
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
